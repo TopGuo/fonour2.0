0009399d8ed4bc0bed4f7999094875e3b9539aa1
Page Larry <zynisacoolman@163.com>
2018-04-18 17:39:45 +0800

    fonour2.0 open source

---
+++ src/fonor2.0.ui/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace fonor2._0.ui.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
